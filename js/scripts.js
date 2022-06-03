d07079f1e89b4d2a965587afe793ad8004f3fc8e
ftolentino <filmer.tolentino@gmail.com>
2022-06-03 14:38:12 -0700

    SETUP initial UI Logic with jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+// Business Logic
+
 //UI Logic
+$(document).ready(function() {
+  $("#survey-form").submit(function(event) {
+    event.preventDefault();
+
+    const colorInput = $("input:radio[name=color]:checked").val();
+    const handInput = $("input:radio[name=hand]:checked").val();
+
+    if (colorInput === 'red' && handInput === 'righty') {
+      $('.output').text('Javascript');
+      $("#result-container").show();
+    } else if ( colorInput === 'blue' && handInput ==='lefty') {
+      $(".output").text('C#');
+      $("#result-container").show();
+    }
+  });
+});
