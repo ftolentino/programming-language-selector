b7ddca6879ee69578396c915426d45420bbecf68
ftolentino <filmer.tolentino@gmail.com>
2022-06-03 16:15:48 -0700

    ADD third branching outcome

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-// Business Logic
+// // Business Logic
+// function outcomeJavascript() {
+
+// }
 
 //UI Logic
 $(document).ready(function() {
@@ -7,12 +10,18 @@ $(document).ready(function() {
 
     const colorInput = $("input:radio[name=color]:checked").val();
     const handInput = $("input:radio[name=hand]:checked").val();
+    const petInput = $("input:radio[name=pet]:checked").val();
+    const clubInput = $("input:radio[name=club]:checked").val();
+    const timeOfDayInput = $("input:radio[name=answer]:checked").val();
 
-    if (colorInput === 'red' && handInput === 'righty') {
+    if (colorInput === 'red' && handInput === 'lefty' && petInput === 'cat' && clubInput === 'liverpool'&& timeOfDayInput === 'yes') {
       $('.output').text('Javascript');
       $("#result-container").show();
-    } else if ( colorInput === 'blue' && handInput ==='lefty') {
+    } else if ( colorInput === 'blue' && handInput ==='righty' && petInput === 'dog' && clubInput === 'man-city' && timeOfDayInput === 'no') {
       $(".output").text('C#');
+      $("#result-container").show();
+    } else if (colorInput === 'red' && handInput ==='lefty'&& petInput === 'cat' && clubInput === 'man-city' && timeOfDayInput === 'yes') {
+      $(".output").text('Python');
       $("#result-container").show();
     }
   });
