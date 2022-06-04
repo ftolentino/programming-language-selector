50d56d3957634001125f51e47be827b6244f0bd9
ftolentino <filmer.tolentino@gmail.com>
2022-06-03 17:02:07 -0700

    REMOVE old variables from submit function

--- js/scripts.js
+++ js/scripts.js
@@ -34,12 +34,6 @@ $(document).ready(function() {
   $("#survey-form").submit(function(event) {
     event.preventDefault();
 
-    const colorInput = $("input:radio[name=color]:checked").val();
-    const handInput = $("input:radio[name=hand]:checked").val();
-    const petInput = $("input:radio[name=pet]:checked").val();
-    const clubInput = $("input:radio[name=club]:checked").val();
-    const timeOfDayInput = $("input:radio[name=answer]:checked").val();
-
     if (javaOutcome()) {
       $('.output').text('Java');
       $("#result-container").show();
