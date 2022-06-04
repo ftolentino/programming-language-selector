9bb8c851c4a5aa337da423248fd06608c512a748
ftolentino <filmer.tolentino@gmail.com>
2022-06-04 16:51:37 -0700

    Move value capture variables back into submit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,26 @@
-// Business Logic
-function javaOutcome() {
-  const colorInput = $("input:radio[name=color]:checked").val();
-  const handInput = $("input:radio[name=hand]:checked").val();
-  const petInput = $("input:radio[name=pet]:checked").val();
-  const clubInput = $("input:radio[name=club]:checked").val();
-  const timeOfDayInput = $("input:radio[name=answer]:checked").val();
-
-  return colorInput === 'red' && handInput === 'lefty' && petInput === 'cat' && clubInput === 'liverpool' && timeOfDayInput === 'yes';
-}
-
-function cSharpOutcome() {
-  const colorInput = $("input:radio[name=color]:checked").val();
-  const handInput = $("input:radio[name=hand]:checked").val();
-  const petInput = $("input:radio[name=pet]:checked").val();
-  const clubInput = $("input:radio[name=club]:checked").val();
-  const timeOfDayInput = $("input:radio[name=answer]:checked").val();
-
-  return colorInput === 'blue' && handInput === 'righty' && petInput === 'dog' && clubInput === 'man-city' && timeOfDayInput === 'no';
-}
-
-function pythonpOutcome() {
-  const colorInput = $("input:radio[name=color]:checked").val();
-  const handInput = $("input:radio[name=hand]:checked").val();
-  const petInput = $("input:radio[name=pet]:checked").val();
-  const clubInput = $("input:radio[name=club]:checked").val();
-  const timeOfDayInput = $("input:radio[name=answer]:checked").val();
-
-  return colorInput === 'red' && handInput === 'righty' && petInput === 'dog' && clubInput === 'liverpool' && timeOfDayInput === 'no';
-}
-
 //UI Logic
 $(document).ready(function() {
   $("#survey-form").submit(function(event) {
     event.preventDefault();
+    const colorInput = $("input:radio[name=color]:checked").val();
+    const handInput = $("input:radio[name=hand]:checked").val();
+    const petInput = $("input:radio[name=pet]:checked").val();
+    const clubInput = $("input:radio[name=club]:checked").val();
+    const timeOfDayInput = $("input:radio[name=answer]:checked").val();
 
-    if (javaOutcome()) {
+    if (colorInput === 'red') {
       $('.output').text('Java');
       $("#result-container").show();
-    } else if (cSharpOutcome()) {
+    } else if (handInput === 'righty') {
       $(".output").text('C#');
       $("#result-container").show();
-    } else if (pythonpOutcome()) {
+    } else if (petInput === 'cat') {
+      $(".output").text('Python');
+      $("#result-container").show();
+    } else if (clubInput === 'liverpool') {
+      $(".output").text('C#');
+      $("#result-container").show();
+    } else if (timeOfDayInput === 'no') {
       $(".output").text('Python');
       $("#result-container").show();
     } else {
