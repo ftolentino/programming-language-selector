d5671e7f867cfca08d2905d101c3e8b245f032fa
ftolentino <filmer.tolentino@gmail.com>
2022-06-04 17:12:35 -0700

    STYLE p tags, UPDATE branching

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,23 @@ $(document).ready(function() {
     const clubInput = $("input:radio[name=club]:checked").val();
     const timeOfDayInput = $("input:radio[name=answer]:checked").val();
 
-    if (colorInput === 'red') {
+    if (colorInput === 'red' && clubInput === 'liverpool') {
       $('.output').text('Java');
       $("#result-container").show();
-    } else if (handInput === 'righty') {
+    } else if (handInput === 'righty' && colorInput === 'blue') {
       $(".output").text('C#');
       $("#result-container").show();
-    } else if (petInput === 'cat') {
+    } else if (petInput === 'cat' && timeOfDayInput === 'no') {
       $(".output").text('Python');
       $("#result-container").show();
-    } else if (clubInput === 'liverpool') {
+    } else if (clubInput === 'man-city' && petInput === 'dog') {
+      $(".output").text('Java');
+      $("#result-container").show();
+    } else if (timeOfDayInput === 'no' && colorInput === 'blue') {
+      $(".output").text('Python');
+      $("#result-container").show();
+    } else if (petInput === 'dog' && colorInput === 'blue') {
       $(".output").text('C#');
-      $("#result-container").show();
-    } else if (timeOfDayInput === 'no') {
-      $(".output").text('Python');
       $("#result-container").show();
     } else {
       $(".output").text('Javascript');
